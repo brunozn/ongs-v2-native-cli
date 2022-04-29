bb00100cae3ef9b9a1269f8d78ed99035a2a6a9a
brunozn <brunojndias@gmail.com>
2022-04-29 17:47:36 -0300

    criando componente estrela

--- src/components/Estrelas.js
+++ src/components/Estrelas.js
@@ -1,35 +1,30 @@
-import React, {useState} from 'react';
-import {Image, StyleSheet, View, TouchableOpacity} from 'react-native';
 
+  
+import React, { useState } from "react";
+import { View, StyleSheet } from 'react-native';
 
-import estrela from '../assets/estrela.png';
-import estrelaCinza from '../assets/estrelaCinza.png';
+import Estrela from './Estrela';
 
 export default function Estrelas({
-    quantidade: quantidadeAntiga, editavel = false, grande = false}) {
-        const [quantidade, setQuantidade] = useState(quantidadeAntiga);
-        const estilos = estilosFuncao(grande);
+    quantidade: quantidadeAntiga,
+    editavel = false,
+    grande = false,
+}) {
+    const [ quantidade, setQuantidade ] = useState(quantidadeAntiga);
 
-        const getImage = (index) => {
-            if(index < quantidade){
-                return estrela;
-            }
-            return estrelaCinza;
+    const RenderEstrelas = () => {
+        const listaEstrelas = [];
+        for (let i = 0; i < 5; i++) {
+            listaEstrelas.push(
+                <Estrela
+                    key={i}
+                    onPress={() => setQuantidade(i + 1)}
+                    desabilitada={!editavel}
+                    preenchida={i < quantidade}
+                    grande={grande} />
+            );
         }
 
-        const RenderEstrelas = () => {
-            const listaEstrelas = [];
-            for (let i=0; i <5; i++){
-                listaEstrelas.push(
-                    <TouchableOpacity
-                        key={i}
-                        onPress={() => setQuantidade(i +1)}
-                        disabled={!editavel}
-                        >
-                            <Image source={getImage(i)} style={estilos.estrela}/>
-                        </TouchableOpacity>
-                );
-        }
         return listaEstrelas;
     }
 
@@ -38,13 +33,8 @@ export default function Estrelas({
     </View>
 }
 
-
-const estilosFuncao = (grande) => StyleSheet.create({
-    estrelas : {
+const estilos = StyleSheet.create({
+    estrelas: {
         flexDirection: 'row',
     },
-    estrela: {
-        width: grande ? 36 : 12,
-        height: grande  ? 36 : 12,
-    }
-})
+});
