e2a1a363f9be9ad261de04a471dba6444d842a05
brunozn <brunojndias@gmail.com>
2022-05-01 00:36:05 -0300

    renomeando pasta

--- src/telas/Home/componentes/Produtor.js
+++ src/telas/Home/componentes/Produtor.js
@@ -1,7 +1,7 @@
 import React, { useReducer, useMemo } from "react";
 import { View, TouchableOpacity, Text, Image, StyleSheet } from "react-native";
 
-import Estrelas from "../../../components/Estrelas";
+import Estrelas from "../../../componentes/Estrelas";
 
 const distanciaEmMetros = (distancia) => {
     return `${distancia}m`;
